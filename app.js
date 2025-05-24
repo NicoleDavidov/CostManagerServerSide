5072d35578f3798038e359b5d216689ebf379af4
Polina <poinkash28@gmail.com>
2025-05-24 13:02:00 +0300

    update

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const connectDB = require('./config/db');
+connectDB(); // קראי לפונקציה כדי להתחבר לבסיס הנתונים
+
+
 var app = express();
 
 // view engine setup
